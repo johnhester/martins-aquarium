52cb686bb37075dd59732bdc65e4439bd1b6389f
John Hester <hesterusmaximus@gmail.com>
2020-06-02 11:19:02 -0500

    completed fish js and generated additional needed JS files

--- scripts/fish/fishData.js
+++ scripts/fish/fishData.js
@@ -3,7 +3,7 @@ const fishCollection = [
         name: 'Angus',
         food: 'Popcorn',
         species: 'Parrot Fish',
-        Location: 'Sea of Google',
+        location : 'Sea of Google',
         length: 8,
         image: 'https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg'
     },
@@ -11,7 +11,7 @@ const fishCollection = [
         name: 'Manny',
         food: 'Mike\'n Ikes',
         species: 'Lionfish',
-        Location: 'Cayman Islands',
+        location : 'Cayman Islands',
         length: 5,
         image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F27ml3ckbz243349t7nkxkpyo-wpengine.netdna-ssl.com%2Fwp-content%2Fuploads%2F2017%2F02%2FDollarphotoclub_72071085.jpg&f=1&nofb=1'
     },
@@ -19,7 +19,7 @@ const fishCollection = [
         name: 'Captain Nemo',
         food: 'Steak Tar Tar',
         species: 'Clownfish',
-        location: 'Some Anenome',
+        location : 'Some Anenome',
         length: 1,
         image: 'http://www.smashinglists.com/wp-content/uploads/2010/02/Oceans-Clownfish.jpg'
     },
@@ -27,7 +27,7 @@ const fishCollection = [
         name: 'Calvin',
         food: 'Kelp Shakes',
         species: 'Cowfish',
-        Location: 'Blue',
+        location : 'Blue',
         length: 3,
         image: 'https://gerolilleike.files.wordpress.com/2013/07/aviary-photo_130173361553213295.png'
     },
@@ -35,7 +35,7 @@ const fishCollection = [
         name: 'Frankie',
         food: 'Dirt',
         species: 'Flounder',
-        Location: 'Probably Below You',
+        location : 'Probably Below You',
         length: 11,
         image: 'http://news.nationalgeographic.com/content/dam/news/2016/08/12/WAQflounders/01WAQflounders.ngsversion.1470952297404.jpg'
     }, 
@@ -43,7 +43,7 @@ const fishCollection = [
         name: 'Jean Baptiste',
         food: 'Grey Poupon',
         species: 'Emporer Angelfish',
-        location: 'Exclusive Fish Nightclubs',
+        location : 'Exclusive Fish Nightclubs',
         length: 8,
         image: 'http://www.caribbean-charter-flights.com/wp-content/uploads/2221.jpg'
     },
@@ -51,7 +51,7 @@ const fishCollection = [
         name: 'David',
         food: 'Dijon Mustard',
         species: 'Blue Tang',
-        location: 'Hanging out with Jean Baptiste',
+        location : 'Hanging out with Jean Baptiste',
         length: 8,
         image: 'http://www.caribbean-charter-flights.com/wp-content/uploads/444.jpg'
     },
@@ -59,7 +59,7 @@ const fishCollection = [
         name: 'Bronson',
         food: 'Stress',
         species: 'Doctorfish',
-        location: 'His Malpractice Lawyer\'s Office',
+        location : 'His Malpractice Lawyer\'s Office',
         length: 6,
         image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F63%2FDoctorfish%252C_Acanthurus_chirurgus.jpg&f=1&nofb=1'
     },
@@ -67,8 +67,8 @@ const fishCollection = [
         name: 'Clarence',
         food: 'The Hearts of His Enemies',
         species: 'Barracude',
-        Location: 'The Water',
+        location : 'The Water',
         length: 10,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/8/81/Grfood_Barracuda_off_the_Netherland_Antilles.jpg'
+        image: 'https://pixabay.com/get/55e2dc464c53b108feda8460ce203f78143cd6f852547848742e7fd79f4a_1920.jpg'
     }
 ]
