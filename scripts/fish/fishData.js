8521cc597d705d0f616ef70659caecda22345304
John Hester <hesterusmaximus@gmail.com>
2020-06-02 10:25:34 -0500

    js stuff

---
+++ scripts/fish/fishData.js
@@ -0,0 +1,74 @@
+const fishCollection = [
+    {
+        name: 'Angus',
+        food: 'Popcorn',
+        species: 'Parrot Fish',
+        Location: 'Sea of Google',
+        length: 8,
+        image: 'https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg'
+    },
+    {
+        name: 'Manny',
+        food: 'Mike\'n Ikes',
+        species: 'Lionfish',
+        Location: 'Cayman Islands',
+        length: 5,
+        image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F27ml3ckbz243349t7nkxkpyo-wpengine.netdna-ssl.com%2Fwp-content%2Fuploads%2F2017%2F02%2FDollarphotoclub_72071085.jpg&f=1&nofb=1'
+    },
+    {
+        name: 'Captain Nemo',
+        food: 'Steak Tar Tar',
+        species: 'Clownfish',
+        location: 'Some Anenome',
+        length: 1,
+        image: 'http://www.smashinglists.com/wp-content/uploads/2010/02/Oceans-Clownfish.jpg'
+    },
+    {
+        name: 'Calvin',
+        food: 'Kelp Shakes',
+        species: 'Cowfish',
+        Location: 'Blue',
+        length: 3,
+        image: 'https://gerolilleike.files.wordpress.com/2013/07/aviary-photo_130173361553213295.png'
+    },
+    {
+        name: 'Frankie',
+        food: 'Dirt',
+        species: 'Flounder',
+        Location: 'Probably Below You',
+        length: 11,
+        image: 'http://news.nationalgeographic.com/content/dam/news/2016/08/12/WAQflounders/01WAQflounders.ngsversion.1470952297404.jpg'
+    }, 
+    {
+        name: 'Jean Baptiste',
+        food: 'Grey Poupon',
+        species: 'Emporer Angelfish',
+        location: 'Exclusive Fish Nightclubs',
+        length: 8,
+        image: 'http://www.caribbean-charter-flights.com/wp-content/uploads/2221.jpg'
+    },
+    {
+        name: 'David',
+        food: 'Dijon Mustard',
+        species: 'Blue Tang',
+        location: 'Hanging out with Jean Baptiste',
+        length: 8,
+        image: 'http://www.caribbean-charter-flights.com/wp-content/uploads/444.jpg'
+    },
+    {
+        name: 'Bronson',
+        food: 'Stress',
+        species: 'Doctorfish',
+        location: 'His Malpractice Lawyer\'s Office',
+        length: 6,
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F63%2FDoctorfish%252C_Acanthurus_chirurgus.jpg&f=1&nofb=1'
+    },
+    {
+        name: 'Clarence',
+        food: 'The Hearts of His Enemies',
+        species: 'Barracude',
+        Location: 'The Water',
+        length: 10,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/8/81/Grfood_Barracuda_off_the_Netherland_Antilles.jpg'
+    }
+]
